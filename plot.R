af0fba3fb06b986472bd4a036f0d9d291d5814a2
wzmli <wzmichael.li@gmail.com>
2020-03-20 22:46:41 -0400

    March 20th update

--- plot.R
+++ plot.R
@@ -6,7 +6,7 @@ library(gridExtra)
 library(colorspace)
 
 label_dat <- (ddtotal
-    %>% filter(Date == as.Date("2020-03-19"))
+    %>% filter(Date == as.Date("2020-03-20"))
     %>% mutate(lab_positives = paste0(Province,":",calcCumCases)
                , lab_total = paste0(Province,":",bestTotal)
                , Date = Date+3)
