7979297bea299247fa1e7995a1ea738ed47e7ed2
wzmli <wzmichael.li@gmail.com>
2020-04-09 01:57:38 -0400

    update plot

--- plot.R
+++ plot.R
@@ -5,6 +5,7 @@ library(ggrepel)
 library(ggforce)
 library(gridExtra)
 library(colorspace)
+library(grid)
 
 ## use url link?
 ddcapacity <- read_csv("capacity.csv")
@@ -15,6 +16,9 @@ label_dat <- (ddclean
     %>% mutate(lab_positives = paste0(Province,":",cumConfirmations)
                , lab_total = paste0(Province,":",bestTotal)
                , lab_newConfirmations = paste0(Province, ":",newConfirmations)
+					, lab_hosp = paste0("Hosp",":",Hospitalization)
+					, lab_icu = paste0("ICU",":",ICU)
+					, lab_vent = paste0("Vent",":",Ventilator)
                , Date = Date+5)
 )
 
@@ -107,37 +111,57 @@ print(gg3
 )
 
 
+hosp_lab_dat <- (label_dat
+	%>% select(Date, Province, Hospitalization=lab_hosp, ICU=lab_icu, Ventilator=lab_vent)
+	%>% gather(key="HospType",value="labs",-Date,-Province)
+	# %>% mutate(Date = Date - 5)
+)
+
+print(hosp_lab_dat)
+
 ## Hospitalization
 ddhosp <- (ddclean
   %>% select(Date, Province, Hospitalization, ICU, Ventilator)
   %>% gather(key="HospType",value="Count",-Date,-Province)
   %>% filter(!is.na(Count)&(Count>0))
   %>% left_join(.,ddcapacity)
+  # %>% left_join(.,hosp_lab_dat)
   %>% mutate(Province = factor(Province,levels = c("BC","AB","ON","QC","SK","MB","NB","NS","PEI","NL","YU","NWT","NU")))
+)
+
+ddhosplab <- (ddhosp
+  %>% filter(Date == as.Date(max(Date)))
+  %>% select(Province,HospType,Count)
+  %>% left_join(hosp_lab_dat,.)
+  %>% ungroup()
+  %>% mutate(Province = factor(Province,levels = c("BC","AB","ON","QC","SK","MB","NB","NS","PEI","NL","YU","NWT","NU"))
+             # , Date = Date - 5
+  )
+  %>% filter(Province %in% c("AB","BC","ON","QC","SK","MB","NS","NL"))
 )
 
 
 
 gghosp <- (ggplot(ddhosp, aes(x=Date, y=Count,color=HospType))
-       + scale_x_date()
-       # + geom_text_repel(data=label_dat,aes(label = lab_positives)
-       #                   , hjust = -10
-       #                   , direction = "y"
-       #                   , size = 3
-       #                   # , nudge_y = 5
-       #                   , segment.color = NA
-       #                   , show.legend = FALSE
-       # )
+		 + geom_text_repel(data=ddhosplab,aes(label = labs)
+                          , hjust = -20
+		                      , vjust= 2
+                          , direction = "y"
+                          , size = 3
+                          # , nudge_y = 5
+                          , segment.color = NA
+                          , show.legend = FALSE
+        )
        + geom_line()
        + geom_point()
-       # + ggtitle(parse(text="'Cumulative Reported'~bold('Positive')~'Tests'"))
 		 + geom_hline(aes(yintercept=Current), color="red",linetype=2)
        + theme(legend.position = "bottom", axis.title.y=element_blank()
                , axis.text.x = element_text(angle = 45,vjust=0.5)
-               , plot.title = element_text(vjust=0,hjust=0.1,size=10))
+               , plot.title = element_text(vjust=0,hjust=0.1,size=10)
+					, strip.background =element_rect(fill="white"))
        + facet_wrap(~Province,nrow=2, scale="free")
        + scale_colour_manual(values=c("black","red","blue"))
-		 + scale_y_log10(breaks=c(1,5,10,30,50,100,200,300,500,600,800))
+		 + scale_y_log10(breaks=c(1,5,10,30,50,100,200,300,600,800))
 
 )
 
